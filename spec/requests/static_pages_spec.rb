4c9a5cfc6ce26d449a3e67d5aa7810c7a7f975f7
面川泰明 <omokawa@senk-inc.co.jp>
2014-05-19 15:16:40 +0900

    postgresql用の設定を追加

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "shoud habe the content 'Sample App'" do
@@ -11,7 +13,7 @@ describe "StaticPages" do
 
     it "should have the right title 'Home' " do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -24,7 +26,7 @@ describe "StaticPages" do
 
     it "should have the right title 'Help' " do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -36,7 +38,20 @@ describe "StaticPages" do
     end
     it "should have the right title 'About Us' " do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
+
+  describe "Contact page" do
+
+    it "shoud habe the content 'Contact' " do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    it "should have the right title 'Contact' " do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+  end
+
 end
