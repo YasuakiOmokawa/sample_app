c06db9cccde6f80a2256c607b6c83e3d74acbe57
面川泰明 <omokawa@senk-inc.co.jp>
2014-05-28 16:09:48 +0900

    ソースの不要コメントを削除

--- app/services/analytics_service.rb
+++ app/services/analytics_service.rb
@@ -6,15 +6,6 @@ require 'yaml'
 
 class AnalyticsService
   def load_profile(user_data)
-    # f = File.open(File.join(Rails.root, 'app','services','conf.yml'))
-    # f = File.open(File.join('app','services','conf.yml'))
-    # conf_str = f.read
-    # myconf = YAML.load(conf_str)
-
-    #先月の初め
-    # start_date = (1.month.ago Time.now).beginning_of_month
-    #先月の終わり
-    # end_date = (1.month.ago Time.now).end_of_month
 
     # セッションログイン
     Garb::Session.login(
@@ -28,23 +19,4 @@ class AnalyticsService
         p.id == user_data.profile_id
       }
   end
-
-  # 指標とディメンションをGarb::Modelをextendしたクラスに定義
-  # class PageTitle
-  #     extend Garb::Model
-  #     metrics :pageviews
-  #     dimensions :date
-  # end
-
-  # cond = {
-  #     :start_date => start_date,
-  #     :end_date   => end_date,
-  #     # :filters    => { :page_path.contains => '^/items/' }
-  # }
-
-  # rs = PageTitle.results(profile, cond)
-  # p rs
-  # rs.each do |r|
-  #     puts r.pageviews # ページビュー
-  # end
 end
