daf0d60fe02d14ac88c3ca46febffbd013bdf536
面川泰明 <omokawa@senk-inc.co.jp>
2014-05-30 08:38:59 +0900

    Rails設定変更
    タイムゾーン: JST
    ロケール： ja
    ロケールファイル：　
    https://github.com/svenfuchs/rails-i18n/blob/master/rails/locale/ja.yml

--- config/application.rb
+++ config/application.rb
@@ -20,10 +20,16 @@ module SampleApp
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    # 表示時のタイムゾーンをJSTに変更
+    config.time_zone = 'Tokyo'
+    # DB保存時のタイムゾーンをJSTに変更
+    config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # ロケールを日本語に変更
+    config.i18n.default_locale = :ja
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
