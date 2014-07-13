87b6f44b3716b2f928a19bff0fbb1f2559484aa4
面川泰明 <omokawa@senk-inc.co.jp>
2014-07-13 10:17:44 +0900

    ブランチイニシャルコミット

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       sign_in user
       redirect_back_or user
     else
-      flash.now[:error] = 'Invalid email/password combination'
+      flash.now[:error] = 'Eメールかパスワードが間違っています'
       render 'new'
     end
   end
