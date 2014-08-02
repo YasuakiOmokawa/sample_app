a7e8ee1b3b00ba23b63b749ffe385ad34f4fe6bd
面川泰明 <omokawa@senk-inc.co.jp>
2014-08-02 18:12:43 +0900

    デプロイ前チェック完了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,11 @@ module ApplicationHelper
   #ページごとの完全なタイトルを返します。
   def full_title(page_title)
     # base_title = "Ruby on Rails Tutorial Sample App"
-    base_title = "TAS for GA"
+    base_title = "AST"
     if page_title.empty?
       base_title
     else
-      "#{base_title} | #{page_title}"
+      "#{page_title} | #{base_title}"
     end
   end
 end
