2eba9ec9f20dc4a10818b14557103d21fb39645e
面川泰明 <omokawa@senk-inc.co.jp>
2014-09-05 12:28:47 +0900

    環境別にタイトルを変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,19 @@ module ApplicationHelper
 
   #ページごとの完全なタイトルを返します。
   def full_title(page_title)
-    # base_title = "Ruby on Rails Tutorial Sample App"
     base_title = "AST"
     if page_title.empty?
       base_title
     else
       "#{page_title} | #{base_title}"
+    end
+  end
+
+  # 環境別にタイトルを変更します。
+  def title_per_env(page_title)
+    env = Rails.env
+    unless Rails.env.production?
+      "#{page_title} | #{env}"
     end
   end
 end
