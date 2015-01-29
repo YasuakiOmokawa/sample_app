ccb0f0ee24b126e709460820c60f4aa1136d6c31
面川泰明 <omokawa@senk-inc.co.jp>
2015-01-29 16:54:20 +0900

    unicornワーカーのチューニング完了
    5worker per 1xdyno でとりあえず運用してみる。

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 require 'action_dispatch/middleware/session/dalli_store'
 
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 8)
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 5)
 timeout 30
 preload_app true
 
