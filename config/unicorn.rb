1d3ceb6203245bea53f5b867fab4bab42261ad6a
面川泰明 <omokawa@senk-inc.co.jp>
2015-05-08 23:26:06 +0900

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 require 'action_dispatch/middleware/session/dalli_store'
 
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 4)
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 30
 preload_app true
 
